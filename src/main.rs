e0296ba69bf66edb337354208b843b90c8ef5b79
hedrickw <selfishtoaster@gmail.com>
2024-03-26 21:46:27 -0700

    add default tracing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use axum::{routing::get, Router, response::Json};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use axum::{error_handling::HandleErrorLayer, response::Json, routing::get, Router,   http::StatusCode};
 use ratchet::component_service;
-use serde_json::{Value,json};
+use serde_json::{json, Value};
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use std::time::Duration;
 #[tokio::main]
 async fn main() {
     // Setup a simple tracing setup
@@ -15,10 +18,27 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(home))
-        .route("/diagnose", get(diagnose_handler));
+        .route("/diagnose", get(diagnose_handler)) // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {error}"),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        );
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
