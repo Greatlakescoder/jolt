f241a37f6e890afce8cd81e5dd1cbcb53dd5d773
root <root@LAPTOP-I0L5KUEN>
2024-03-30 20:34:27 -0700

    for tmrw

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use axum::{
-    error_handling::HandleErrorLayer, http::StatusCode, response::Json, routing::get,
-    routing::post, Router,
+    error_handling::HandleErrorLayer, extract::Extension, http::StatusCode, response::Json,
+    routing::get, routing::post, Router,
 };
 use ratchet::component_service;
 
 use ratchet::file_service::*;
 use serde_json::{json, Value};
-use std::sync::{Arc, Mutex, mpsc::*};
+use std::sync::{
+    mpsc::{channel, Sender},
+    Arc, Mutex,
+};
 use std::time::Duration;
 use tokio::task;
 use tower::{BoxError, ServiceBuilder};
 use tower_http::trace::TraceLayer;
+use tower_http::add_extension::AddExtensionLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+struct AppState {
+    channel_sender: Arc<Mutex<Sender<String>>>,
+}
+
 #[tokio::main]
 async fn main() {
     // Setup a simple tracing setup
@@ -23,6 +32,11 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let (tx, rx) = channel();
+    let app_state = Arc::new(AppState {
+        channel_sender: Arc::new(Mutex::new(tx)),
+    });
+
     let app = Router::new()
         .route("/", get(home))
         .route("/diagnose", get(diagnose_handler))
@@ -30,6 +44,7 @@ async fn main() {
         .route("/info/memory", get(ram_info_handler))
         .route("/search", post(search)) // Add middleware to all routes
         .route("/file/largest", post(get_largest_file))
+        .layer(AddExtensionLayer::new(app_state))
         .layer(
             ServiceBuilder::new()
                 .layer(HandleErrorLayer::new(|error: BoxError| async move {
@@ -47,6 +62,13 @@ async fn main() {
                 .into_inner(),
         );
 
+    // Spawn a new task to read from the channel
+    let read_task = tokio::spawn(async move {
+        while let Ok(message) = rx.recv() {
+            println!("Received: {}", message);
+        }
+    });
+
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
@@ -63,12 +85,21 @@ async fn diagnose_handler() -> Json<Value> {
     return Json(json!(resp));
 }
 
-async fn cpu_info_handler() -> Json<Value> {
-    let resp = match task::spawn_blocking(move || component_service::get_current_cpu_usage()).await {
+async fn cpu_info_handler(Extension(app_state): Extension<Arc<AppState>>) -> Json<Value> {
+    let channel_sender = match app_state.channel_sender.lock() {
+        Ok(sender) => sender,
+        Err(err) => {
+            return Json(json!({"error": format!("Failed to acquire lock: {:?}", err)}));
+        }
+    };
+
+    // channel_sender.send("Hello from home handler".to_string()).unwrap();
+    let resp = match task::spawn_blocking(move || component_service::get_current_cpu_usage()).await
+    {
         Ok(result) => result,
         Err(e) => {
             return Json(json!({ "error": format!("Error in spawn_blocking: {:?}", e) }));
-        },
+        }
     };
 
     return Json(json!(resp));
@@ -112,20 +143,22 @@ async fn get_largest_file(Json(payload): Json<SearchRequest>) -> Json<Value> {
         //     println!("Update: {}", received);
         // }
         return r;
-    }).await {
+    })
+    .await
+    {
         Ok(Ok(resp)) => resp,
         Ok(Err(e)) => {
             return Json(json!({ "error": format!("Error in find_largest_files: {:?}", e) }));
-        },
+        }
         Err(e) => {
             return Json(json!({ "error": format!("Error in spawn_blocking: {:?}", e) }));
-        },
+        }
     };
     let data_vault = match resp.lock() {
         Ok(data) => data,
         Err(e) => {
             return Json(json!({ "error": format!("Error locking mutex: {:?}", e) }));
-        },
+        }
     };
     return Json(json!(*data_vault));
 }
